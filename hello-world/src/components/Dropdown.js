f1399e224cb1c8fca120c3acce8173367fe26be3
Pritam78459 <pritamkhamaru33@gmail.com>
2021-02-01 13:52:23 +0530

    practical 2 added

---
+++ hello-world/src/components/Dropdown.js
@@ -0,0 +1,22 @@
+import React , {Component, useState} from 'react';
+
+const Dropdown = () => {
+    const [greeting, setGreeting] = useState("Greetings");
+
+    const createText = (element) => {
+        setGreeting(element.target.value)
+    }
+
+    return (
+        <div>
+            <p style={{display: "inline-block"}}> Greetings : {greeting}</p>
+            <select onChange={createText.bind(this)}>
+                <option value="Hi">Hi</option>
+                <option value="Hello">Hello</option>
+                <option value="Hey">Hey</option>
+            </select>
+        </div>
+    );
+}
+
+export default Dropdown;
